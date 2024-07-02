47937003cc4b710cc35d8dbd061522d1c2415b0e
D-Ayub <diolog66@gmail.com>
2024-07-02 19:49:14 +0300

    web-site library
    library

---
+++ script.js
@@ -0,0 +1,286 @@
+document.getElementById("Open-modal-btn").addEventListener("click", function() {
+    document.getElementById("my-modal").classList.add("open");
+});
+
+document.getElementById("close-my-modal-btn").addEventListener("click", function() {
+    document.getElementById("my-modal").classList.remove("open");
+});
+
+document.getElementById("Open-modal-btn12").addEventListener("click", function() {
+    document.getElementById("my-modal12").classList.add("open");
+});
+
+document.getElementById("close-my-modal-btn12").addEventListener("click", function() {
+    document.getElementById("my-modal12").classList.remove("open");
+});
+
+document.getElementById("Open-modal-btn13").addEventListener("click", function() {
+    document.getElementById("my-modal13").classList.add("open");
+});
+
+document.getElementById("close-my-modal-btn13").addEventListener("click", function() {
+    document.getElementById("my-modal13").classList.remove("open");
+});
+
+document.getElementById("Open-modal-btn14").addEventListener("click", function() {
+    document.getElementById("my-modal14").classList.add("open");
+});
+
+document.getElementById("close-my-modal-btn14").addEventListener("click", function() {
+    document.getElementById("my-modal14").classList.remove("open");
+});
+
+document.getElementById("Open-modal-btn15").addEventListener("click", function() {
+    document.getElementById("my-modal15").classList.add("open");
+});
+
+document.getElementById("close-my-modal-btn15").addEventListener("click", function() {
+    document.getElementById("my-modal15").classList.remove("open");
+});
+
+document.getElementById("Open-modal-btn16").addEventListener("click", function() {
+    document.getElementById("my-modal16").classList.add("open");
+});
+
+document.getElementById("close-my-modal-btn16").addEventListener("click", function() {
+    document.getElementById("my-modal16").classList.remove("open");
+});
+
+document.getElementById("Open-modal-btn17").addEventListener("click", function() {
+    document.getElementById("my-modal17").classList.add("open");
+});
+
+document.getElementById("close-my-modal-btn17").addEventListener("click", function() {
+    document.getElementById("my-modal17").classList.remove("open");
+});
+
+document.getElementById("Open-modal-btn18").addEventListener("click", function() {
+    document.getElementById("my-modal18").classList.add("open");
+});
+
+document.getElementById("close-my-modal-btn18").addEventListener("click", function() {
+    document.getElementById("my-modal18").classList.remove("open");
+});
+
+document.getElementById("Open-modal-btn19").addEventListener("click", function() {
+    document.getElementById("my-modal19").classList.add("open");
+});
+
+document.getElementById("close-my-modal-btn19").addEventListener("click", function() {
+    document.getElementById("my-modal19").classList.remove("open");
+});
+
+document.getElementById("Open-modal-btn20").addEventListener("click", function() {
+    document.getElementById("my-modal20").classList.add("open");
+});
+
+document.getElementById("close-my-modal-btn20").addEventListener("click", function() {
+    document.getElementById("my-modal20").classList.remove("open");
+});
+
+document.getElementById("Open-modal-btn21").addEventListener("click", function() {
+    document.getElementById("my-modal21").classList.add("open");
+});
+
+document.getElementById("close-my-modal-btn21").addEventListener("click", function() {
+    document.getElementById("my-modal21").classList.remove("open");
+});
+
+document.getElementById("Open-modal-btn22").addEventListener("click", function() {
+    document.getElementById("my-modal22").classList.add("open");
+});
+
+document.getElementById("close-my-modal-btn22").addEventListener("click", function() {
+    document.getElementById("my-modal22").classList.remove("open");
+});
+
+document.getElementById("Open-modal-btn23").addEventListener("click", function() {
+    document.getElementById("my-modal23").classList.add("open");
+});
+
+document.getElementById("close-my-modal-btn23").addEventListener("click", function() {
+    document.getElementById("my-modal23").classList.remove("open");
+});
+
+document.getElementById("Open-modal-btn24").addEventListener("click", function() {
+    document.getElementById("my-modal24").classList.add("open");
+});
+
+document.getElementById("close-my-modal-btn24").addEventListener("click", function() {
+    document.getElementById("my-modal24").classList.remove("open");
+});
+
+document.getElementById("Open-modal-btn25").addEventListener("click", function() {
+    document.getElementById("my-modal25").classList.add("open");
+});
+
+document.getElementById("close-my-modal-btn25").addEventListener("click", function() {
+    document.getElementById("my-modal25").classList.remove("open");
+});
+
+document.getElementById("Open-modal-btn26").addEventListener("click", function() {
+    document.getElementById("my-modal26").classList.add("open");
+});
+
+document.getElementById("close-my-modal-btn26").addEventListener("click", function() {
+    document.getElementById("my-modal26").classList.remove("open");
+});
+
+document.getElementById("Open-modal-btn27").addEventListener("click", function() {
+    document.getElementById("my-modal27").classList.add("open");
+});
+
+document.getElementById("close-my-modal-btn27").addEventListener("click", function() {
+    document.getElementById("my-modal27").classList.remove("open");
+});
+
+document.getElementById("Open-modal-btn28").addEventListener("click", function() {
+    document.getElementById("my-modal28").classList.add("open");
+});
+
+document.getElementById("close-my-modal-btn28").addEventListener("click", function() {
+    document.getElementById("my-modal28").classList.remove("open");
+});
+
+document.getElementById("Open-modal-btn29").addEventListener("click", function() {
+    document.getElementById("my-modal29").classList.add("open");
+});
+
+document.getElementById("close-my-modal-btn29").addEventListener("click", function() {
+    document.getElementById("my-modal29").classList.remove("open");
+});
+
+document.getElementById("Open-modal-btn30").addEventListener("click", function() {
+    document.getElementById("my-modal30").classList.add("open");
+});
+
+document.getElementById("close-my-modal-btn30").addEventListener("click", function() {
+    document.getElementById("my-modal30").classList.remove("open");
+});
+
+document.getElementById("Open-modal-btn31").addEventListener("click", function() {
+    document.getElementById("my-modal31").classList.add("open");
+});
+
+document.getElementById("close-my-modal-btn31").addEventListener("click", function() {
+    document.getElementById("my-modal31").classList.remove("open");
+});
+
+document.getElementById('books').addEventListener('click', function(e) {
+    e.preventDefault();
+    
+    const booksSection = document.getElementById('book');
+    
+    booksSection.scrollIntoView({ behavior: 'smooth' });
+});
+
+document.getElementById('konts').addEventListener('click', function(e) {
+    e.preventDefault();
+    
+    const booksSection = document.getElementById('kont');
+    
+    booksSection.scrollIntoView({ behavior: 'smooth' });
+});
+
+document.getElementById('glaws').addEventListener('click', function(e) {
+    e.preventDefault();
+    
+    const booksSection = document.getElementById('glaw');
+    
+    booksSection.scrollIntoView({ behavior: 'smooth' });
+});
+
+let texts = ["Хасавюртовская центральная городская библиотека имени Расула Гамзатова, основанная в 1921 году, стала свидетельницей трех эпох: зарождения СССР, расцвета и распада Советского Союза, зарождения и развития новой независимой России. Судьба библиотеки неотделима от судьбы страны и ее непростой истории. Библиотека как государственное учреждение вынуждена была принимать участие в различных партийных кампаниях, в зачастую оторванных от реалий жизни пропагандистских акциях. Но даже при существовавшей жесткой регламентации местные библиотекари, как свидетельствуют архивные документы и газетные публикации прошлых лет, делали все возможное, чтобы удовлетворить повседневные читательские запросы горожан. Такой внешний конформизм, с одной стороны, обеспечил библиотеке выживаемость в критические моменты, а с другой, способствовал росту ее авторитета у жителей Хасавюрта.","Деятельность библиотеки в 1990–2000-е годы разворачивалась на фоне печальных, а подчас и трагических событий. Главное библиотечное здание трижды оказывалось в эпицентре разрушительных террористических актов. Хасавюртовские библиотекари просто не могли оставаться в стороне от сложных общественно-политических процессов и активно включились в решение проблем этнических меньшинств, людей с ограниченными физическими возможностями, вынужденных мигрантов, подростков, попавших в трудную жизненную ситуацию. Очень часто эта деятельность осуществляется в формах, которые, может быть, не совсем вписываются в традиционные рамки библиотечной работы. Но, считая, что публичная библиотека — это сила, прежде всего направленная на благо общества, библиотекари Хасавюрта принимали и впредь намерены принимать все возможные конструктивные меры, которые способны реально улучшить жизнь сограждан.", "Последние десять лет библиотека Хасавюрта реализует программу «Формирование культуры межэтнического общения», которая рассматривается как важнейший инструмент преодоления этнической нетерпимости. Идея программы состоит в том, что обмен духовными ценностями, знакомство с достижениями культуры других народов значительно обогащает личность. Одним из проектов программы стала открытая при библиотеке воскресная школа национального языка и литературы для подростков из семей этнических меньшинств. С ними проводились специальные интерактивные занятия по освоению родных языков. Ведь, чтобы понимать чужую культуру, человек должен быть открыт к культуре отечественной. Формируя позитивную этническую идентичность, библиотека в то же время развивала у подростков толерантное отношение к другим этническим группам. Эта задача решалась посредством расширения доступа не только к культурным ценностям своего народа, но и к достижениям культуры и традиций других народов, их верованиям и обычаям. Последовавшие за этим проектом другие библиотечные акции, с одной стороны, создавали условия для стабильного существования малых этнических групп местного населения, а, с другой, содействовали мирному межкультурному взаимодействию в полиэтническом сообществе.", "Муниципальный бюджет стабильно финансирует все законодательно установленные статьи библиотечных расходов, а на проекты городского масштаба библиотека получает дополнительные средства из специального фонда руководителя города. Книжные коллекции последние пять лет увеличиваются в среднем на тысячу томов в год, библиотека имеет возможность выписывать широкий спектр периодики. Пополняется и собрание документов на электронных носителях: что-то приобретается самостоятельно, но большую часть этих материалов дарят библиотеке благодарные читатели. Однако ведь библиотека — это не только документный фонд, это еще и библиотекари, высокий уровень профессионализма которых местная власть поддерживает стимулирующими надбавками к зарплате за стаж и качество работы. Ежегодно библиотечные специалисты за счет муниципальных субсидий участвуют с докладами и сообщениями в крупнейших международных, общероссийских и региональных библиотечных форумах, проходят краткосрочные стажировки в федеральных и крупных региональных библиотеках. Это прекрасная школа повышения квалификации — библиотекари возвращаются домой с новыми знаниями, новыми идеями и большим желанием улучшить библиотечно-информационное обслуживание горожан."];
+let currentIndex = 0;
+
+function showPrevText() {
+  currentIndex = (currentIndex - 1 + texts.length) % texts.length;
+  document.getElementById("modalText").innerText = texts[currentIndex];
+}
+
+function showNextText() {
+  currentIndex = (currentIndex + 1) % texts.length;
+  document.getElementById("modalText").innerText = texts[currentIndex];
+}
+
+
+
+document.getElementById("books").addEventListener("click", () => {
+    if (navigation.classList.contains("active")) {
+        navigation.classList.remove("active");
+        setTimeout(() => {
+            navigationItems.classList.add("hide");
+        }, 100); 
+    }
+});
+
+window.addEventListener("scroll", () => {
+    if (navigation.classList.contains("active")) {
+        navigation.classList.remove("active");
+    }
+});
+
+const menuBtn = document.querySelector(".menu-btn");
+const navigation = document.querySelector(".navigation");
+const navigationItems = document.querySelector(".navigation-items");
+
+menuBtn.addEventListener("click", () => {
+    menuBtn.classList.toggle("active");
+    navigation.classList.toggle("active");
+});
+
+function toggleMenu() {
+    var menuBtn = document.querySelector(".menu-btn");
+    var img = document.querySelector(".menu-btn.active");
+
+    if (menuBtn.classList.contains("active")) {
+        menuBtn.classList.remove("active");
+        img.classList.remove("hide");
+    } else {
+        menuBtn.classList.add("active");
+        img.classList.add("hide");
+    }
+}
+
+window.addEventListener("scroll", function() {
+    var menuBtn = document.querySelector(".menu-btn");
+    var img = document.querySelector(".menu-btn.active");
+
+    if (menuBtn.classList.contains('active')) {
+        menuBtn.classList.remove('active');
+        img.classList.remove('hide');
+    }
+});
+
+document.getElementById('eye').addEventListener('click', function() {
+    document.getElementById('glaw').classList.toggle('black-background');
+    var img = this.querySelector('img');
+    if (img.src.includes('visibility_off_FILL0_wght400_GRAD0_opsz24.png')) {
+        img.src = 'visibility_FILL0_wght400_GRAD0_opsz24.png';
+    } else {
+        img.src = 'visibility_off_FILL0_wght400_GRAD0_opsz24.png';
+    }
+});
+
+const productContainers = [...document.querySelectorAll('.product-container')];
+const nxtBtn = [...document.querySelectorAll('.nxt-btn')];
+const preBtn = [...document.querySelectorAll('.pre-btn')];
+
+productContainers.forEach((item, i) => {
+    let containerDimensions = item.getBoundingClientRect();
+    let containerWidth = containerDimensions.width;
+
+    nxtBtn[i].addEventListener('click', () => {
+        item.scrollLeft += containerWidth;
+    })
+
+    preBtn[i].addEventListener('click', () => {
+        item.scrollLeft -= containerWidth;
+    })
+})
+
+
+function openModal(modalId) {
+    var modal = document.getElementById(modalId);
+    modal.style.display = 'block';
+}
